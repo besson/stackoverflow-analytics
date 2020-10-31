5cb3d5227b1b7c6a02eb2a67f50e6643c3b2544e
Felipe Besson <felipe.besson@olx.com>
2020-10-31 10:15:50 +0100

    part 2 of analysis

--- data_helpers.py
+++ data_helpers.py
@@ -1,4 +1,4 @@
-def bucketize_last_hire_date(last_hire_date) -> str:
+def bucketize_last_hire_date(last_hire_date) -> str :
     '''
     INPUT - last hire data answer
     OUTPUT - a discrete value assigned to each answer
@@ -11,3 +11,17 @@ def bucketize_last_hire_date(last_hire_date) -> str:
     }
 
     return buckets.get(last_hire_date, -1)
+
+
+def transform_coding_experience(experience) -> float :
+    '''
+    INPUT - experience answer in years
+    OUTPUT - years in a numeric field
+    '''
+
+    conversion = {
+        'Less than 1 year': 0.5,
+        'More than 50 years': 51
+    }
+
+    return conversion.get(experience) if experience in conversion else float(experience)
